de6c94e7519de7c0a4bf0193f8efd99a2513863b
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-09 15:27:45 +0100

    Elenco email

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,14 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 const app = new Vue({
   el: "#app",
   data: {
-    userEmail: "",
-    userEmailTwo: "",
+    mails: [""],
   },
   mounted() {
     axios
       .get("https://flynn.boolean.careers/exercises/api/random/mail")
       .then((response) => {
         console.log(response.data.response);
-        this.userEmail = response.data.response;
+        this.mails = response.data.response;
       });
   },
 });
