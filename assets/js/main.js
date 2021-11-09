905e89d31404601bbe0a2722ed2508acd05028e5
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-09 15:54:38 +0100

    Ciclo for mails

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,14 +7,22 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 const app = new Vue({
   el: "#app",
   data: {
-    mails: [""],
+    mails: [],
   },
+  methods()
+     {
+    function cicloFor(axios){
+        for(let i > 0; mails.lenght < 10; i++)
+        return true;
+    }
+
   mounted() {
     axios
       .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((response) => {
-        console.log(response.data.response);
-        this.mails = response.data.response;
+      .then((result) => {
+        console.log(result.data.response);
+        this.mails.push(result.data.response);
+        
       });
   },
 });
